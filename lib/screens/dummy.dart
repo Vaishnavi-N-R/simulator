c2b4e2f6af8c3b45e559fb6de8305ee1c9c19fd5
Vaishnavi-veni <vaishnavinr06@gmail.com>
2024-10-21 17:13:36 +0530

    after slight modification

--- lib/screens/dummy.dart
+++ lib/screens/dummy.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trade_simulator/device/device_utility.dart';
+import 'package:trade_simulator/widgets/appbar/appbar.dart';
+import 'package:trade_simulator/widgets/drawer/drawer.dart';
 
 class Dummy1 extends StatefulWidget {
   const Dummy1({super.key});
@@ -10,7 +13,25 @@ class Dummy1 extends StatefulWidget {
 class _Dummy1State extends State<Dummy1> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Homeeeeeeeeee"),);
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(TDeviceUtils.getAppBarHeight()),
+        child: Builder(
+          builder: (context) => TAppBar(
+            leadingIcon: Icons.menu, // Pass the leading icon
+            leadingOnPressed: () {
+              // Open the drawer when the leading icon is pressed
+              Scaffold.of(context).openDrawer();
+            },
+            title: const Text('Home Page'), // Set the title of the AppBar
+          ),
+        ),
+      ),
+      drawer: const CustomDrawer(), // Use your custom drawer
+      body: Center(
+        child: Text("Homeeeeeeeee"),
+      ),
+    );
   }
 }
 
@@ -24,6 +45,24 @@ class Dummy2 extends StatefulWidget {
 class _Dummy2State extends State<Dummy2> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Settingssssss"),);
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(TDeviceUtils.getAppBarHeight()),
+        child: Builder(
+          builder: (context) => TAppBar(
+            leadingIcon: Icons.menu, // Pass the leading icon
+            leadingOnPressed: () {
+              // Open the drawer when the leading icon is pressed
+              Scaffold.of(context).openDrawer();
+            },
+            title: const Text('Home Page'), // Set the title of the AppBar
+          ),
+        ),
+      ),
+      drawer: const CustomDrawer(), // Use your custom drawer
+      body: Center(
+        child: Text("Settingssssss"),
+      ),
+    );
   }
 }
