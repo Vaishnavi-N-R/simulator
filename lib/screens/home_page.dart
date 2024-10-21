453dc8de9739a8a15fb1013697f22ba6157ef4e6
Vaishnavi-veni <vaishnavinr06@gmail.com>
2024-10-21 13:13:58 +0530

    Drawer

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:trade_simulator/utils/navigation_menu.dart';
 import 'package:trade_simulator/widgets/appbar/appbar.dart';
+import 'package:trade_simulator/widgets/drawer/drawer.dart';// Import CustomDrawer
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,10 +13,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Use the CustomDrawer widget here
+      drawer: const CustomDrawer(),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TAppBar(leadingIcon: Icons.menu,actions: [Drawer()],)
+            // Wrap TAppBar with Builder to provide the correct context
+            Builder(
+              builder: (BuildContext context) {
+                return TAppBar(
+                  leadingIcon: Icons.menu,
+                  leadingOnPressed: () {
+                    Scaffold.of(context).openDrawer(); // Now it will work
+                  },
+                  title: const Text('Home Page'),
+                );
+              },
+            ),
+            // Other content goes here
           ],
         ),
       ),
---
+++ lib/widgets/drawer/drawer.dart
@@ -0,0 +1,43 @@
+// widgets/custom_drawer.dart
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Drawer Header',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pop(context); // Close the drawer
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+              Navigator.pop(context); // Close the drawer
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
