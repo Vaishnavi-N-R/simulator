2aac6e653a10a06c2d00ebaa161afb037073ce97
Vaishnavi-veni <vaishnavinr06@gmail.com>
2024-10-22 10:42:46 +0530

    final starter

--- lib/theme/custom_themes/appbar_theme.dart
+++ lib/theme/custom_themes/appbar_theme.dart
@@ -4,26 +4,24 @@ class TAppBarTheme {
   TAppBarTheme._();
 
   static const lightAppBarTheme = AppBarTheme(
-    elevation: 0,
-    centerTitle: false,
-    scrolledUnderElevation: 0,
-    backgroundColor: Colors.transparent,
-    surfaceTintColor: Colors.transparent,
-    iconTheme: IconThemeData(color: Colors.black, size: 24),
-    actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
-    titleTextStyle: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w600,color: Colors.black)
-  );
+      elevation: 0,
+      centerTitle: false,
+      scrolledUnderElevation: 0,
+      backgroundColor: Color.fromARGB(255, 168, 162, 162),
+      surfaceTintColor: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.black, size: 24),
+      actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
+      titleTextStyle: TextStyle(
+          fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.black));
 
-
-   static const darkAppBarTheme = AppBarTheme(
-    elevation: 0,
-    centerTitle: false,
-    scrolledUnderElevation: 0,
-    backgroundColor: Colors.transparent,
-    surfaceTintColor: Colors.transparent,
-    iconTheme: IconThemeData(color: Colors.black, size: 24),
-    actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
-    titleTextStyle: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w600,color: Colors.black)
-
-  );
+  static const darkAppBarTheme = AppBarTheme(
+      elevation: 0,
+      centerTitle: false,
+      scrolledUnderElevation: 0,
+      backgroundColor: Color.fromARGB(255, 69, 68, 68),
+      surfaceTintColor: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.white, size: 24),
+      actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
+      titleTextStyle: TextStyle(
+          fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white));
 }
